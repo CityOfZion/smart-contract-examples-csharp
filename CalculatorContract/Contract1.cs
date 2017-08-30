25872e044aa4a572cc95382ca2fce9929dea7aa8
birmas <birmingh@gmail.com>
2017-08-31 01:55:32 +1000

    updated parameter editor and added calculator

---
+++ CalculatorContract/Contract1.cs
@@ -0,0 +1,57 @@
+﻿using Neo.SmartContract.Framework;
+using Neo.SmartContract.Framework.Services.Neo;
+using System;
+using System.Numerics;
+
+namespace CalculatorContract
+{
+    /**
+     * simple calculator supporting add/subtract
+     * param 05 (string) add|sub
+     * object[] int, int
+     */
+    public class Contract1 : FunctionCode
+    {
+        public static void Main(string operation, params object[] args)
+        {
+            int arg0 = (int)args[0];
+            int arg1 = (int)args[1];
+            int result = -1;
+
+            Runtime.Notify("CalculatorContract Init", arg0, arg1);
+
+            if (operation == "add")
+            {
+                result = CalculatorAdd(arg0, arg1);
+            }
+
+            if(operation == "sub")
+            {
+                result = CalculatorSub(arg0, arg1);
+            }
+
+            Runtime.Notify("Calculator", operation, result);
+            return;
+        }
+
+        static private int CalculatorSub(int a, int b)
+        {
+            int sum = a - b;
+
+            Runtime.Notify("CalculatorSub Received", a, b);
+            Runtime.Notify("CalculatorSub Result", sum);
+
+            return sum;
+        }
+
+        static private int CalculatorAdd(int a, int b)
+        {
+            int sum = a + b;
+
+            Runtime.Notify("CalculatorAdd Received", a, b);
+            Runtime.Notify("CalculatorAdd Result", sum);
+
+            return sum;
+        }
+    }
+}
--- ContractParameterTest/Contract1.cs
+++ ContractParameterTest/Contract1.cs
@@ -5,51 +5,72 @@ using System.Numerics;
 
 namespace ContractParameterTest {
     public class Contract1 : FunctionCode {
+
+        //[Appcall("d4e86a20af4cd4508de71c57bb211db8b1688a28")]
+        //public static extern int CalculatorContract(string operation, params object[] args);
         /**
          * helper contract to test the parameter editor in neo-developer-gui. Output added to "event log" tab of gui
-         * contract available on testnet at: f491a081ac6532d580c67fab8bbed4e5ad0ada82
+         * contract available on testnet at: 
          * 
-         * param operation (05) should be one of: Boolean|Integer|ByteArray|Signature|Hash160|Hash256|PublicKey|String
-         * param args must contain a single argument of the type matching operation
+         * param operation (05) should be one of: Boolean|Integer|ByteArray|Signature|Hash160|Hash256|PublicKey|Array|Calculator
+         * param args must contain a single argument of the type matching operation unless you have used Calculator in which case you must provide string(add|sub), int, int
          */
-        public static void Main(string operation, params object[] args) {
+        public static object Main(string operation, params object[] args) {
 
             Runtime.Log("Operation: " + operation);
 
+            if(operation == "Calculator")
+            {
+                Runtime.Log("Not supported yet");
+                //CalculatorContract((string)args[0], (int)args[1], (int)args[2]);
+                return true;
+            }
+
             if (operation == "Boolean") {
                 bool boolArg = (bool)args[0];
-                if (boolArg) {
-                    Runtime.Log("Operation: Boolean Value: true");
-                } else {
-                    Runtime.Log("Operation: Boolean Value: false");
-                }
-                return;
+                Runtime.Notify("Operation: Boolean Value: ", boolArg);
+                return args[0];
             }
 
             if (operation == "Integer") {
                 Runtime.Log("processing Integer: " + args[0]);
                 BigInteger intArg = (BigInteger)args[0];
-                Runtime.Notify("Operation: Integer Value: ..", intArg);
-                return;
+                Runtime.Notify("Operation: Integer Value: ", intArg);
+                return args[0];
             }
 
             if (operation == "ByteArray" || operation == "Signature") {
                 Runtime.Log("processing " + operation);
                 Runtime.Notify(operation, BytesToHex((byte[])args[0]));
-                return;
+                return args[0];
             }
 
             if (operation == "Hash160" || operation == "Hash256" || operation == "PublicKey") {
                 Runtime.Notify(operation, (byte[])args[0]);
-                return;
+                return args[0];
             }
 
-            if (operation == "String") {
-                Runtime.Log("Operation: String Value: " + args[0]);
-                return;
+            if(operation == "Array")
+            {
+                object[] arrayObject = (object[])args[0];
+                Runtime.Notify(operation, arrayObject.Length);
+                return arrayObject.Length;
             }
 
             Runtime.Log("Unhandled operation: " + operation);
+            return false;
+        }
+
+        private static byte[] IntToBytes(BigInteger value)
+        {
+            byte[] buffer = value.ToByteArray();
+            return buffer;
+        }
+
+        private static BigInteger BytesToInt(byte[] array)
+        {
+            var buffer = new BigInteger(array);
+            return buffer;
         }
 
         private static string BytesToHex(byte[] bytes) {
